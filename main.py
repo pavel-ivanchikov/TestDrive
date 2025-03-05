c92f5d0cbf7e9b86db082efce9b78f444fcf4f07
pavel-ivanchikov <pavel.ivanchikov@gmail.com>
2025-03-05 13:07:47 +0700

    добавил возможность изменять файл и упростил аунтетификацию

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import os
 
+# Инициализация GoogleAuth
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+
+# Проверяем, существует ли уже сохраненный токен
+if os.path.exists('token.json'):
+    gauth.LoadCredentialsFile('token.json')  # Загружаем сохраненные учетные данные
+else:
+    # Если токена нет, запускаем полный процесс авторизации
+    gauth.LocalWebserverAuth()
+    gauth.SaveCredentialsFile('token.json')  # Сохраняем токен для будущих запусков
+
+# Инициализация GoogleDrive
+drive = GoogleDrive(gauth)
+
+
+def change_file(file_name='test.txt', additional_content='hey dude!', folder_id=None):
+    try:
+        # Поиск файла по имени и папке
+        query = f"title = '{file_name}' and trashed = false"
+        if folder_id:
+            query += f" and '{folder_id}' in parents"
+
+        file_list = drive.ListFile({'q': query}).GetList()
+
+        if not file_list:
+            return f"File '{file_name}' not found in folder '{folder_id}'."
+
+        # Получаем первый найденный файл (предполагаем, что файл с таким именем один)
+        my_file = file_list[0]
+
+        # Читаем текущее содержимое файла
+        current_content = my_file.GetContentString()
+
+        # Добавляем новое содержимое
+        updated_content = current_content + "\n" + additional_content
+
+        # Обновляем файл
+        my_file.SetContentString(updated_content)
+        my_file.Upload()
+
+        return f"File '{file_name}' was successfully updated in folder '{folder_id}'."
+    except Exception as ex:
+        return f'Got some troubles: {str(ex)}'
 
 
 def create_and_upload_file(file_name='test.txt', file_content='hey dude!', folder_id=None):
     try:
-        drive = GoogleDrive(gauth)
 
         file_metadata = {'title': f'{file_name}'}
         if folder_id:
@@ -24,7 +65,12 @@ def create_and_upload_file(file_name='test.txt', file_content='hey dude!', folde
 
 def main():
     folder_id = '1tw7XLgqcdxDTLidZdXumd1DMdRRCiHYH'
-    print(create_and_upload_file('my_file.txt', 'Hello World!', folder_id))
+
+    # Создаем и загружаем файл
+    print(create_and_upload_file('my_file1.txt', 'Hello World!', folder_id))
+
+    # Изменяем файл
+    print(change_file('my_file1.txt', 'This is additional content.', folder_id))
 
 
 if __name__ == '__main__':
